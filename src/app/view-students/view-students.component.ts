f7d6d8ce13f1b6b4dcc7402d187660937f985a9c
Adhimalu <adhimalu678@gmail.com>
2022-07-08 12:06:58 +0530

    student_view updated

--- src/app/view-students/view-students.component.ts
+++ src/app/view-students/view-students.component.ts
@@ -11,5 +11,12 @@ export class ViewStudentsComponent implements OnInit {
 
   ngOnInit(): void {
   }
-name="Ammu"
+data=[
+  {"Name":"Raju","Age":20,"photo":"https://www.vexels.com/free-vectors/avatar/"},
+  {"Name":"Nikhil","Age":21,"photo":"https://www.vexels.com/free-vectors/avatar/"},
+  {"Name":"Immu","Age":22,"photo":"https://www.vexels.com/free-vectors/avatar/"},
+  {"Name":"Ani","Age":10,"photo":"https://www.vexels.com/free-vectors/avatar/"},
+  {"Name":"Sona","Age":11,"photo":"https://www.vexels.com/free-vectors/avatar/"},
+  {"Name":"Gayathri","Age":25,"photo":"https://www.vexels.com/free-vectors/avatar/"}
+]
 }
